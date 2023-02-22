bcd679ba0dd349e95a69231da2bf26ea3a795e18
Patricio Sandor <patriciosandor@gmail.com>
2023-02-22 19:41:04 -0300

    Proyecto adaptado para RENDER

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ const path = require("path");
 
 app.use(express.static(path.resolve(__dirname, "../public")))
 
+const port= process.env.PORT || 3000;
 
-app.listen(3000, ()=> console.log("Server corriendo en puerto 3000"))
+app.listen(port, ()=> console.log("Server corriendo en puerto 3000"))
 
 app.get("/", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/home.html")))
 
